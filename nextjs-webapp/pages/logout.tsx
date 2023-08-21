3adcc8f319fb6930e70b1b88bb90393cd1390b32
dazzy <41763728+OmegaMaxy@users.noreply.github.com>
2023-08-21 14:25:39 +0200

    Fixed logout page

--- nextjs-webapp/pages/logout.tsx
+++ nextjs-webapp/pages/logout.tsx
@@ -2,10 +2,22 @@ import { signOut } from "next-auth/react";
 import { useEffect } from "react";
 import { Box } from "@chakra-ui/react"
 import { useRouter } from "next/router";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "./api/auth/[...nextauth]";
+import type { GetServerSidePropsContext } from "next";
 
 export default function Page() {
     const router = useRouter()
-    signOut()
+    //signOut()
+
+    useEffect(() => {
+        async function a() {
+            const data = await signOut({ redirect: false, callbackUrl: "/" })
+            router.push(data.url)
+        } 
+        a()
+
+    }, [])
     
     //router.push('/')
 
