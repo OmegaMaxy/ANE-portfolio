2aa4228561d9e81cd3a79052fbe2d6a35ddff479
dazzy <41763728+OmegaMaxy@users.noreply.github.com>
2023-08-21 15:13:28 +0200

    Updated sidebarlayout.tsx

--- nextjs-webapp/components/layouts/sidebarlayout.tsx
+++ nextjs-webapp/components/layouts/sidebarlayout.tsx
@@ -3,12 +3,13 @@ import Navbar from "../Navbar"
 import ChakraLink from "../core/Link"
 import ButtonLink from "../core/ButtonLink"
 import Sidebar from "../Sidebar"
+import { useColorModeValue } from "@chakra-ui/react"
 
 export default function Layout({ children }) {
     return (
-        <HStack bg="#e1e1e1" h="100vh">
+        <HStack bg={useColorModeValue("#e1e1e1", "chakra-body-bg")} h="100vh">
             <Sidebar w="15%" />
-            <Box w="85%" h="100%" p="10" bg="white">
+            <Box w="85%" h="100%" p="10" bg={useColorModeValue("white", "chakra-body-bg")}>
                 {children}
             </Box>
         </HStack>
