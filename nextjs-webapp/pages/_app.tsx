fdd5701e08236c66701c70b305fc778c3780e1fd
dazzy <dazzy.edu@gmail.com>
2023-08-24 00:31:40 +0200

    Implemented auth check

--- nextjs-webapp/pages/_app.tsx
+++ nextjs-webapp/pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
-import { SessionProvider } from "next-auth/react"
+import { ChakraProvider, Spinner } from '@chakra-ui/react'
+import { SessionProvider, useSession } from 'next-auth/react'
 import '../public/main.css'
 
 import theme from '../lib/theme'
@@ -17,18 +17,42 @@ type AppPropsWithLayout = AppProps<{ session: Session }> & {
 }
 
 
-function App({ Component, pageProps: { session, ...pageProps} }: AppPropsWithLayout) {
+function App({ Component, router, pageProps: { session, ...pageProps} }: AppPropsWithLayout) {
 
     const getLayout = Component.getLayout ?? ((page) => page)
     return (
         <SessionProvider session={session}>
             <ChakraProvider theme={theme}>
                 {getLayout(
-                <Component {...pageProps}/>
+                    Component.auth ? (
+                        <Auth auth={Component.auth} router={router}>
+                            <Component {...pageProps} key={router.route} />
+                        </Auth>
+                    ) : (
+                        <Component {...pageProps} key={router.route} />
+                    )
                 )}
             </ChakraProvider>
         </SessionProvider>
     )
+}
+
+function Auth({ children, auth, router }: any) {
+    const { status, data } = useSession({
+        required: true, onUnauthenticated() {
+            router.push(auth.unauthenticated ? auth.unauthenticated : '/not-logged-in')
+        }
+    })
+    if (status === 'loading') {
+        return (auth.loading) ? auth.loading : <Spinner />
+    }
+
+    if (!data || !data.user) return null // unauthenticated
+
+    /*if (data?.user.role != auth.role) {
+        router.push(auth.unauthorized)
+    }*/
+    return children
 }
 
 export default App
